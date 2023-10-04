300f34b1df4a72ab6e0ef08f62f09c3618e63023
Andreas Claesson <mexus81@mexus.se>
2023-10-04 13:44:05 +0200

    Fixat påpekningar på Js koden

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 localStorage.setItem("username", "Bella"); 
 localStorage.setItem("password", "qwe123"); 
+const visaHuvud = document.querySelector(".huvud"); // Första huvudfönstret
+const inLoggad = document.querySelector(".inloggad"); //Andra huvudfönstret
 let enteredUsername;
 // Delen med att lagra namn,lösen, samt en varibel som går att komma åt beroende på var man är i koden.
 // Nedanför börjar hanteringen av formuläret när det skickas. Ett stort "block" sköter detta.
@@ -28,8 +30,6 @@ document.getElementById("uppgifter").addEventListener("submit", function(form) {
 });
 // Huvudfunktionen vad som ska göras vid hos en anv. som har lyckats med sin inloggning.
 function loggedIn(){
-    const visaHuvud = document.querySelector(".huvud"); // Första huvudfönstret
-    const inLoggad = document.querySelector(".inloggad"); //Andra huvudfönstret
     localStorage.setItem("trueFalse", true); /* Denna sköter en viktig uppgift 
     om man är utloggad/inloggad vid stängning av sin webbläsare */
         if (enteredUsername) {
@@ -49,20 +49,25 @@ function loggedIn(){
         inLoggad.style.display = "block";
         inLoggad.appendChild(logoutButton);
     //Skapar en utloggningsknapp till index.html när man är inloggad. Sätter vilket fönster som ska visas när man är inloggad.
+    document.getElementById("utLoggad").addEventListener("click", function () {
+        loggaUt();
+    });
 
-    document.getElementById("utLoggad").addEventListener("click", function()
-    {
+    }
+
+    function loggaUt() {
+
         localStorage.setItem("trueFalse", false);
         visaHuvud.style.display = "block";
         inLoggad.style.display = "none";
-        logoutButton.remove();
         location.reload();
         console.log("Du loggades ut.")
-    });
+    }
+
  /* Koden ovanför hanterar utloggningen. Vilka fönster ska visas efter samt en "refresh". 
  Sätter värdet i "trueFalse" till false för första gången med eftersom man har gjort ett 
  medvetet val att logga ut.*/
-}
+
 // Nedanför är en funktion som kollar vid öppning av sidan igen om man är inloggad eller inte.
 window.addEventListener("load", function () {
     const kontroll = localStorage.getItem("trueFalse");
@@ -70,11 +75,8 @@ window.addEventListener("load", function () {
         enteredUsername = localStorage.getItem("username");
         loggedIn();
     }
-    else {
-        localStorage.setItem("trueFalse", false);
-        visaHuvud.style.display = "block";
-        inLoggad.style.display = "none";
-        location.reload();
-        console.log("Du loggades ut.")  
+    else if 
+        (kontroll !== "false") {
+         loggaUt();
     }
 });
